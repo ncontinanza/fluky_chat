1c126d48915d4d5b0f4a4c5a32bad57c335d0998
NContinanza <nicolas.continanza@entropy1729.com>
2022-10-26 12:31:30 -0300

    Initialize project

---
+++ lib/fluky_chat.ex
@@ -0,0 +1,18 @@
+defmodule FlukyChat do
+  @moduledoc """
+  Documentation for `FlukyChat`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FlukyChat.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
